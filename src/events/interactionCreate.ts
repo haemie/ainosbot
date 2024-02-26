90678ac64bd0c43c9837cf606f5f6e01c08d7771
haemie <jmezhang@gmail.com>
2024-02-25 22:04:54 -0600

    added proper typing for objects

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -9,7 +9,19 @@ module.exports = {
     }
 
     // interaction.client.commands holds all commands
-    const command = (interaction.client as newClient).commands.get(interaction.commandName);
+    /** retreive command function definition from the commands Collection on the Client object
+     * based on the commandname passed with the interactionCreate command
+     *
+     * i.e. user issues command /ping,
+     * creates an InteractionCreate event,
+     * gets triggered by event listener created in index.ts,
+     * callback to event is the execute() function in this file
+     * execute() function in this file grabs command name "ping" and grabs execution for ping
+     * runs ping execute() function
+     */
+    const command = (interaction.client as newClient).commands.get(
+      interaction.commandName
+    );
     // interaction.client.commands: Collection(5) [Map] {
     //   'ping' => { data: [SlashCommandBuilder], execute: [AsyncFunction: execute] },
     //   'server' => { data: [SlashCommandBuilder], execute: [AsyncFunction: execute] },
