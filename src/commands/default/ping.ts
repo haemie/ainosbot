58139fc67613a3cdbf4394c179f662767fb2ead7
haemie <jmezhang@gmail.com>
2024-02-26 18:02:34 -0600

    added latency to ping command

--- src/commands/default/ping.ts
+++ src/commands/default/ping.ts
@@ -3,8 +3,25 @@ import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setDescription('Replies with Pong!')
+
+    .addBooleanOption((option) =>
+      option
+        .setName('ephemeral')
+        .setDescription('whether or not echo should be ephemeral')
+    ),
+
   async execute(interaction: CommandInteraction) {
-    await interaction.reply('Pong!');
+    // console.log(targetuser);
+    // console.log(interaction.member);
+
+    // ephemeral to make message only visible to sender
+    // console.log(interaction);
+    const ping = Date.now() - interaction.createdTimestamp;
+
+    await interaction.reply({
+      content: `Pong! *server took ${ping}ms to receive message*`,
+      ephemeral: false,
+    });
   },
 };
