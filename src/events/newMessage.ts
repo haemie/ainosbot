7c868c78e78b17d953d95f7d7e1399aeecf4979d
haemie <jmezhang@gmail.com>
2024-03-06 16:38:14 -0600

    separate tables with separate message per scout

--- src/events/newMessage.ts
+++ src/events/newMessage.ts
@@ -1,13 +1,13 @@
 import { Events, Message } from 'discord.js';
 import { scoutMessage } from '../util/types';
 
-function createMessage(obj: scoutMessage) {
+function createMessage(obj: string[]) {
   let res = '';
-  for (const [key, value] of Object.entries(obj)) {
-    let valuestring = '';
-    res += `# ${key}:\n${(value as string[]).join('\n')}\n`;
-  }
-  return res;
+  obj.forEach((e) => {
+    res += `${e}\n`;
+  });
+
+  return '```' + res + '```';
 }
 
 const returnMessage: scoutMessage = {
@@ -23,6 +23,28 @@ const returnMessage: scoutMessage = {
 
 let lastScout: Message | null = null;
 
+type lastScoutsType = {
+  top1: Message | null;
+  top2: Message | null;
+  mid1: Message | null;
+  mid2: Message | null;
+  bot1: Message | null;
+  bot2: Message | null;
+  sh1: Message | null;
+  sh2: Message | null;
+};
+
+const lastScouts: lastScoutsType = {
+  top1: null,
+  top2: null,
+  mid1: null,
+  mid2: null,
+  bot1: null,
+  bot2: null,
+  sh1: null,
+  sh2: null,
+};
+
 module.exports = {
   name: Events.MessageCreate,
   async execute(message: Message) {
@@ -35,31 +57,72 @@ module.exports = {
         const key = e as keyof scoutMessage;
         returnMessage[key].splice(0);
       }
-      lastScout = null;
+      for (let e of Object.keys(lastScouts)) {
+        const key = e as keyof lastScoutsType;
+        lastScouts[key] = null;
+      }
       await message.reply('cleared');
     } else if (messageContent[0] in returnMessage) {
       // console.log(message.author);
       // there should be scout data in this message now
-      if (lastScout) {
+      if (
+        messageContent[0] in lastScouts &&
+        lastScouts[messageContent[0] as keyof typeof lastScouts]
+      ) {
         // check if there's a previous message to delete first
-        await lastScout.delete();
-      }
-
-      if (message.attachments) {
-        message.attachments.forEach((e) => {
-          messageContent.push(e.proxyURL);
-        });
+        await lastScouts[
+          messageContent[0] as keyof typeof lastScouts
+        ]?.delete();
       }
 
       // save the target for command
       const key = messageContent[0] as keyof scoutMessage;
 
       // manipulate return message string
-      messageContent[0] = message.author.toString();
-      returnMessage[key].push(messageContent.join(' '));
+      // remove command name from return string
+      messageContent.shift();
+
+      // split command string into its lines
+      const lines = messageContent.join(' ').split('\n');
+
+      // add a header to the table:
+      lines.unshift('Hero|HP|Speed|Artifact|Other');
+
+      const rows = lines.map((line) => line.split('|'));
+
+      // we now have an array of rows holding an array of columns each. column index width must be the same
+
+      const colLengths = Array(5).fill(0);
+
+      rows.forEach((ro, i) =>
+        ro.forEach((co, j) => {
+          colLengths[j] = Math.max(co.length, colLengths[j]);
+        })
+      );
+
+      const paddedText = rows
+        .map((ro, i) =>
+          ro.map((co, j) => co.padEnd(colLengths[j], ' ')).join(' | ')
+        )
+        .join('\n');
+
+      // add images from the user to the bot post
+      let messageAttachments = '';
+      if (message.attachments) {
+        message.attachments.forEach((e) => {
+          messageAttachments += e.proxyURL;
+        });
+      }
+
+      returnMessage[key].push(paddedText);
 
       lastScout = await message.reply({
-        content: createMessage(returnMessage),
+        content:
+          key +
+          ' SCOUT BY ' +
+          message.author.toString() +
+          createMessage(returnMessage[key]) +
+          messageAttachments,
         allowedMentions: {
           parse: [], // ping nobody
           repliedUser: false, // don't ping the replied user
